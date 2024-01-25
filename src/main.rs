a42e8fad4b0e7bbcbc50e86b10c0a640cc63b1b6
Evgenii <jenyhlevnoy@gmail.com>
2024-01-25 21:09:21 +0200

    third task version 1

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,30 @@
 use std::{env, fs};
 
 trait DirWalker {
-    fn find(&self, path: String, file_to_find: String);
+    fn find(&self, path: &String, file_to_find: &String);
 }
 
 struct FileFinder;
 
 impl DirWalker for FileFinder {
-    fn find(&self, path: String, file_to_find: String) {
-        match fs::read_dir(&path) {
+    fn find(&self, path: &String, file_to_find: &String) {
+        match fs::read_dir(path) {
             Err(why) => println!("! {:?}", why.kind()),
             Ok(paths) => {
-                for found_path in paths {
-                    if let Ok(entry) = found_path {
-                        let name = entry.file_name();
-                        let cur_path = entry.path();
+                for path in paths {
+                    if let Ok(entry) = path {
+                        let name = entry.file_name().to_string_lossy().to_string();
 
-                        if cur_path.is_file() {
+                        if entry.path().is_file() {
                             if file_to_find.is_empty() {
-                                println!("> {}", name.to_string_lossy());
+                                println!("> {}", name);
                             }
-                            if name.to_string_lossy().to_string() == file_to_find {
-                                println!("Found file at: {:?}", cur_path);
+                            if name == *file_to_find {
+                                println!("Found file at: {:?}", entry.path());
                                 return;
                             }
                         } else {
-                            self.find(cur_path.to_string_lossy().to_string(), file_to_find.clone())
+                            self.find(&name, file_to_find);
                         }
                     }
                 }
@@ -39,10 +38,10 @@ fn main() {
 
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
-        file_finder.find(args[1].clone(), String::new())
+        file_finder.find(&args[1], &String::new());
     } else if args[2] == "--find" {
-        file_finder.find(args[1].clone(), args[3].clone())
+        file_finder.find(&args[1], &args[3]);
     } else {
-        eprintln!("Error: no --find option!")
+        eprintln!("Error: no --find option!");
     }
 }
