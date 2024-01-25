616a4d66c04a771402862d3feab984c5d7c5131a
Evgenii <jenyhlevnoy@gmail.com>
2024-01-25 22:37:08 +0200

    third task version 2

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,31 @@
 use std::{env, fs};
 
 trait DirWalker {
-    fn find(&self, path: &String, file_to_find: &String);
+    fn find(&self, path: &str, file_to_find: &str);
 }
 
 struct FileFinder;
 
 impl DirWalker for FileFinder {
-    fn find(&self, path: &String, file_to_find: &String) {
-        match fs::read_dir(path) {
+    fn find(&self, path: &str, file_to_find: &str) {
+        match fs::read_dir(&path) {
             Err(why) => println!("! {:?}", why.kind()),
             Ok(paths) => {
                 for path in paths {
                     if let Ok(entry) = path {
-                        let name = entry.file_name().to_string_lossy().to_string();
+                        let name = entry.file_name();
+                        let name_str = name.to_string_lossy();
 
                         if entry.path().is_file() {
                             if file_to_find.is_empty() {
-                                println!("> {}", name);
+                                println!("> {}", name_str);
                             }
-                            if name == *file_to_find {
+                            if name_str == file_to_find {
                                 println!("Found file at: {:?}", entry.path());
                                 return;
                             }
                         } else {
-                            self.find(&name, file_to_find);
+                            self.find(&name_str, file_to_find)
                         }
                     }
                 }
@@ -38,7 +39,7 @@ fn main() {
 
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
-        file_finder.find(&args[1], &String::new());
+        file_finder.find(&args[1], "");
     } else if args[2] == "--find" {
         file_finder.find(&args[1], &args[3]);
     } else {
