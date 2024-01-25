f3e380285b3b0d6bc5a748ca093ece9398ed38df
Evgenii <jenyhlevnoy@gmail.com>
2024-01-26 00:23:38 +0200

    5th task version 1

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,36 @@
-use std::{env, fs};
+use std::{env, fs, io::Write};
+
+trait OutputStrategy {
+    fn execute(&self, file_list: &[String]);
+}
+
+struct ConsoleOutput;
+
+impl OutputStrategy for ConsoleOutput {
+    fn execute(&self, file_list: &[String]) {
+        for file in file_list {
+            println!("{}", file);
+        }
+    }
+}
+
+struct FileOutput {
+    file_name: String,
+}
+
+impl OutputStrategy for FileOutput {
+    fn execute(&self, file_list: &[String]) {
+        let mut file = fs::File::create(&self.file_name).unwrap();
+        for file_name in file_list {
+            writeln!(file, "{}", file_name).unwrap();
+        }
+    }
+}
+
 
 trait DirWalker {
     fn find(&self, base_path: &str, path: &str, file_to_find: &str, file_list: &mut Vec<String>);
-    fn print_files(&self, sort_flag: bool, file_list: &mut Vec<String>);
+    fn print_files(&self, sort_flag: bool, file_list: &mut Vec<String>, strategy: &dyn OutputStrategy);
 }
 
 struct FileFinder;
@@ -45,36 +73,40 @@ impl DirWalker for FileFinder {
         }
     }
 
-    fn print_files(&self, sort_flag: bool, file_list: &mut Vec<String>) {
+    fn print_files(&self, sort_flag: bool, file_list: &mut Vec<String>, strategy: &dyn OutputStrategy) {
         if sort_flag {
             bubble_sort(file_list);
         }
-        for file in file_list {
-            println!("{}", file);
-        }
+        strategy.execute(file_list);
     }
 }
 
 fn main() {
     let file_finder = FileFinder;
-
     let args: Vec<String> = env::args().collect();
     let base_path = &args[1];
-    let mut file_to_find = "";
+
+    let default_file_to_find = String::new();
+    let mut file_to_find: &str = &default_file_to_find;
     let mut sort_flag = false;
-    let mut file_list: Vec<String> = Vec::new();
+    let mut output_strategy: Box<dyn OutputStrategy> = Box::new(ConsoleOutput);
 
     for i in 0..args.len() {
         if args[i] == "--find" {
-            if i + 1 < args.len() {
-                file_to_find = &args[i + 1];
-            }
+            file_to_find = args.get(i + 1).unwrap_or(&default_file_to_find);
         }
         if args[i] == "--sort" {
             sort_flag = true;
         }
+        if args[i] == "-f" {
+            if i + 1 < args.len() {
+                output_strategy = Box::new(FileOutput { file_name: args[i + 1].clone() });
+            }
+        }
     }
 
+    let mut file_list: Vec<String> = Vec::new();
     file_finder.find(base_path, base_path, file_to_find, &mut file_list);
-    file_finder.print_files(sort_flag, &mut file_list);
+    file_finder.print_files(sort_flag, &mut file_list, &*output_strategy);
 }
+
